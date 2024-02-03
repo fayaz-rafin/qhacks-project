d65653265e5d5d8554290c932dac33cc1a3a90f0
fayaz-rafin <fayaz.rafin@gmail.com>
2024-02-03 11:40:18 -0500

    create the react-app

---
+++ receiptreturn/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          I know way too many people here right now that I didn't know last year
+          Who the fuck are y'all?          
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
