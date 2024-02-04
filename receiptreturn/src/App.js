a0f2d01711c7294c3415014ae95d02d268989af0
fayaz-rafin <fayaz.rafin@gmail.com>
2024-02-04 04:50:27 -0500

    still working on POST reqs

--- receiptreturn/src/App.js
+++ receiptreturn/src/App.js
@@ -92,7 +92,7 @@ export default function App() {
                     </div>
                 </div>
                 <button onClick={() => setShowImage(true)} className="items-center justify-center px-12 py-3 text-base font-medium text-white bg-purple-700 border border-transparent rounded-md shadow-sm hover:bg-indigo-700">Add receipts</button>
-                {showImage && <img className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]" alt="receipt" />}
+                {showImage && <img src="https://images-ext-2.discordapp.net/external/Rp411oR4mpyjvVmIK3T3sVwvZ7Pb8CUNwgpCVzy9DjM/https/cs21003200207f14c17.blob.core.windows.net/receipts/1/a9481f14-f703-4dff-94fc-31cec02a23f8sample_receipt.jpeg?format=webp&width=372&height=1068" className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]" alt="receipt" />}
                 <div className="-ml-12 -mt-12 p-12  lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
                     <img
                         className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
