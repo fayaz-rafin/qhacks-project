844d16c131640b31f5f5253231fe9ae0cb6c4635
fayaz-rafin <fayaz.rafin@gmail.com>
2024-02-04 03:03:26 -0500

    added POST request in homepage, but it doesnt work

--- receiptreturn/src/App.js
+++ receiptreturn/src/App.js
@@ -60,7 +60,7 @@ export default function App() {
                         id="formFile"
                     />
                 </div>
-                <button className="  items-center justify-center px-12 py-3 text-base font-medium text-white bg-purple-700 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 ">Add receipts</button>
+                <button onClick={sendPostRequest} className="  items-center justify-center px-12 py-3 text-base font-medium text-white bg-purple-700 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 ">Add receipts</button>
                 <div className="-ml-12 -mt-12 p-12  lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
                     <img
                         className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
